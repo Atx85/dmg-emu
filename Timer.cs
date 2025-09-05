897adc2d674d22df687a2677fbf47a2ad7d69ca8
asus small <attila@asussmall.com>
2025-09-05 17:48:08 +0100

    still timer

--- Timer.cs
+++ Timer.cs
@@ -4,30 +4,32 @@ namespace GB {
     private ushort lastCounter = 0;
     private Bus bus;
 
+    private byte tima = 0;
+    private  byte tma = 0;
+    private byte tac = 0;
+
+    private bool overflowPending = false;
+    private int overflowDelay = 0;
+
     public Timer (Bus bus) {
       this.bus = bus;
-      // Initialize DIV to the high byte of 0
-      bus.Write(0xFF04, 0);
+      systemCounter = 0;
     }
 
     public void Tick(int cycles) {
       lastCounter = (ushort)systemCounter;
       systemCounter = (systemCounter + (uint)cycles) & 0xFFFF;
 
-      // Update DIV (high 8 bits of systemCounter)
-      bus.Write(0xFF04, (byte)(systemCounter >> 8));
-
-      byte tac = bus.Read(0xFF07);
       bool timerEnabled = (tac & 0x04) != 0;
       if (timerEnabled) {
         int bit; 
         int sel = (tac & 0x03); 
         switch (sel) {
-         case  0: bit = 9; break;  // 4096 Hz
-         case  1: bit = 3; break;  // 262144 Hz
-         case  2: bit = 5; break;  // 65536 Hz
-         case  3:  // 16384 Hz
-         default: bit = 7; break; 
+          case  0: bit = 9; break;  // 4096 Hz
+          case  1: bit = 3; break;  // 262144 Hz
+          case  2: bit = 5; break;  // 65536 Hz
+          case  3:  // 16384 Hz
+          default: bit = 7; break; 
         };
 
         bool prev = ((lastCounter >> bit) & 1) != 0;
@@ -38,29 +40,34 @@ namespace GB {
           IncrementTIMA();
         }
       }
+
+      if (overflowPending) {
+        if (--overflowDelay == 0) {
+          tima = tma;
+          byte flags = bus.Read(0xFF0F);
+          bus.Write(0xFF0F, (byte)(flags | 0x04));
+          overflowPending = false;
+        }
+      }
     }
+
     public byte ReadDIV() => (byte)(systemCounter >> 8);
     public void WriteDIV(byte val) { systemCounter = 0; }
-  
-    public byte ReadTIMA() => bus.Read(0xFF05);
-    public void WriteTIMA(byte val) => bus.Write(0xFF05, val);
 
-    public byte ReadTMA() => bus.Read(0xFF06);
-    public void WriteTMA(byte val) => bus.Write(0xFF06, val);
-   
-    public byte ReadTAC() => bus.Read(0xFF07);
-    public void WriteTAC(byte val) => bus.Write(0xFF07, val);
+    public byte ReadTIMA() => tima;
+    public void WriteTIMA(byte val) { tima = val; }
+
+    public byte ReadTMA() => tma;
+    public void WriteTMA(byte val) { tma = val;} 
+
+    public byte ReadTAC() => tac;
+    public void WriteTAC(byte val) {tac = val; } 
     private void IncrementTIMA() {
-      byte tima = bus.Read(0xFF05);
       if (tima == 0xFF) {
-        // overflow: reload from TMA, then set IF bit 2
-        bus.Write(0xFF05, bus.Read(0xFF06));
-
-        // Delay: one machine cycle effect- can be safely omitted for Blargg tests
-        byte flags = bus.Read(0xFF0F);
-        bus.Write(0xFF0F, (byte)(flags | 0x04));
+        overflowPending = true;
+        overflowDelay = 1;
       } else {
-        bus.Write(0xFF05, (byte)(tima + 1));
+        tima++;
       }
     }
   }
