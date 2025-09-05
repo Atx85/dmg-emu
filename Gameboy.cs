6af7b05df5af28f378c481ec3a91f807ddf86a1a
asus small <attila@asussmall.com>
2025-09-05 17:54:42 +0100

    still timer

--- Gameboy.cs
+++ Gameboy.cs
@@ -9,14 +9,14 @@ namespace GB {
       var cart = new Cartridge();
       cart.load(path);
       var bus = new Bus(ref cart);
-      var timer = new Timer(bus);
       Cpu cpu = new Cpu(bus);
       int i = 10000;
       while(true) {
         i--;
         int cycles = cpu.Step();
           // ppu.Tick(cycles);
-        timer.Tick(cycles);
+        for (int c = 0; c < cycles; c++)
+          bus.timer.Tick(cycles);
           // cpu.handleInterrupts();
          // if (cpu.eiPending) {
          //   cpu.IME = true;
